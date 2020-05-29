40be252c823f72881361921d78951156e933c3f7
Edward Navarro <edward@unicauca.edu.co>
2020-05-29 15:40:01 -0500

    Configuracion de GULP con PUG y SASS

--- public/js/scripts-min.js
+++
@@ -1,1 +0,0 @@
-"use strict";var numbers=[1,2,3,4],numberDouble=numbers.map((function(e){return 2*e}));console.log(numbers);
--- src/js/index.js
+++
@@ -1,4 +0,0 @@
-const numbers = [1,2,3,4]
-const numberDouble = numbers.map(number => number*2)
-
-console.log(numbers)
